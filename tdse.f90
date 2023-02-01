cdac160746070975fc6d4c463fc50c326f93025b
Toby Nguyen <tobyvn@congo.ps.uci.edu>
2023-01-31 16:58:04 -0800

    Cleaned up code, should be working again

--- tdse.f90
+++ tdse.f90
@@ -8,7 +8,7 @@ program tdse
        & pi = 4d0*atan(1d0)
   double complex :: temp
   double complex, allocatable :: psi(:), psi0(:), transmat(:,:), &
-       & psi_tilda(:) ! psi_norm normalize
+       & psitilda(:) ! psitilda is wavefunction in real orthonormal basis
   double precision, allocatable :: ham(:,:), tkin(:,:), &
        & vpot(:), u(:,:), statevec(:,:), work(:), omega(:)
   double precision, external :: dznrm2
@@ -24,9 +24,9 @@ program tdse
   t = 0d0
   tau = 1d0
 
-  !! Allocating  higher order tensors
+  !! Allocating higher order tensors
   allocate (psi(n),psi0(n),ham(2,n),tkin(2,n),vpot(n),u(n,n), &
-       & work(3*n),omega(n),statevec(n,n),transmat(n,n), psi_tilda(n))
+       & work(3*n),omega(n),statevec(n,n),transmat(n,n),psitilda(n))
   
   !! Discretizing initial wavepacket
   imz0sq = 0.25d0*(p0/alpha)**2
@@ -48,18 +48,20 @@ program tdse
      print *, 'Normalized wavepacket norm =',psinorm
   end if
 
-  ! Transform to orthonormal basis
+  !! Transforming to real orthonormal basis
   temp = dcmplx(sqrt(h),0d0)
   do i = 1,n
-     psi_tilda(i) = psi0(i)*temp
+     psitilda(i) = psi0(i)*temp ! rescale to make basis orthonormal
   end do
-  psi_tilda(1) = psi_tilda(1)/dcmplx(sqrt(2d0),0d0)
-  psi_tilda(n) = psi_tilda(n)/dcmplx(sqrt(2d0),0d0)
-  print *, 'Norm of the actual size: ', dznrm2(n,psi_tilda,1)
-
+  psitilda(1) = psitilda(1)/dcmplx(sqrt(2d0),0d0)
+  psitilda(n) = psitilda(n)/dcmplx(sqrt(2d0),0d0)
+  if (idebug > 0) then
+     print *, 'Norm in real orthonormal basis =', dznrm2(n,psitilda,1)
+  end if
+  
   !! Defining Hamiltonian operator
   vpot = 0d0 ! local potential at grid points
-  hinv = 1d0 / (h*h)
+  hinv = 1d0/(h*h)
   t_loop_diagonal: do i = 1,n
      tkin(1,i) = hinv ! diagonal
      tkin(2,i) = -0.5d0*hinv ! offdiagonal
@@ -70,6 +72,7 @@ program tdse
      ham(2,i) = tkin(2,i)
   end do h_loop
 
+  !! Diagonalizing Hamiltonian operator
   omega = 0d0
   statevec = 0d0
   call dsbev('v','l',n,1,ham,2,omega,statevec,n,work,info)
@@ -77,70 +80,70 @@ program tdse
      print *, "Info = ", info
      stop 'error in dsbev'
   end if
-
-!!$  print *, "Static Hamiltonian eigenvalues :", omega
-!!$  do i=1,n
-!!$     write (88,*) dble(i-1)*h - 1d0, statevec(i,n)
-!!$  end do
-
-  ! Transform wavefunction from position to energy eigenbasis
   do i = 1, n
      do j = 1, n
-        transmat(i,j) = dcmplx(statevec(i,j),0d0)
+        transmat(i,j) = dcmplx(statevec(i,j),0d0) ! make statevec dcmplx
      end do
   end do
-  
-  ! call zgemv('t',n,n,dcmplx(1d0,0d0),transmat,n,psi0,1,dcmplx(0d0,0d0),psi,1)
+
+  !! Graphing eigenstates
+  ! print *, "Static Hamiltonian eigenvalues :", omega
+  ! do i=1,n
+  !    write (88,*) dble(i-1)*h - 1d0, statevec(i,n)
+  ! end do
+
+  !! Transforming wavefunction to energy eigenbasis
   do i = 1, n
      psi(i) = dcmplx(0d0,0d0)
      do j = 1, n
-        psi(i) = psi(i)+transmat(j,i)*psi_tilda(j)
+        psi(i) = psi(i)+transmat(j,i)*psitilda(j) ! transformation
      end do
   end do
-  
-  print *, 'Norm in energy basis ', dznrm2(n,psi,1)
-  
-  ! Multiplying wavefunction by a diagonal time propagator
+  ! call zgemv('t',n,n,dcmplx(1d0,0d0),transmat,n,psi0,1,dcmplx(0d0,0d0),psi,1)
+  if (idebug > 0) then
+     print *, 'Norm in energy basis =', dznrm2(n,psi,1)
+  end if
+     
+  !! Propagating momentum wavefunction by one time step
   do i = 1, n
-     psi(i) = exp(-dcmplx(0d0,1d0)*omega(i)*tau)*psi(i)
+     psi(i) = exp(-dcmplx(0d0,1d0)*omega(i)*tau)*psi(i) ! propagation
   end do
-
-  print *, 'Norm after one time step in energy basis ', dznrm2(n,psi,1)
+  if (idebug > 0) then
+     print *, 'Norm after time step in energy basis =', dznrm2(n,psi,1)
+  end if
   
-  ! Transform wavefunction back to position basis
-  ! call zgemv('n',n,n,dcmplx(1d0,0d0),transmat,n,psi,1,dcmplx(0d0,0d0),psi0,1)
+  !! Transforming wavefunction back to real orthonormal basis
   do i = 1, n
-     psi_tilda(i) = dcmplx(0d0,0d0)
+     psitilda(i) = dcmplx(0d0,0d0)
      do j = 1, n
-        psi_tilda(i) = psi_tilda(i)+transmat(j,i)*psi(j)
+        psitilda(i) = psitilda(i)+transmat(j,i)*psi(j)
      end do
   end do
+  ! call zgemv('n',n,n,dcmplx(1d0,0d0),transmat,n,psi,1,dcmplx(0d0,0d0),psi0,1)
+  if (idebug > 0) then
+     print *, 'Norm after time step in real orthonormal basis =', dznrm2(n,psitilda,1)
+  end if
   
-  print *, 'Norm after one time step in position basis ', dznrm2(n,psi,1)
-  
-  ! Deallocation of higher order tensors
-  deallocate (psi,psi0,ham,tkin,vpot,u,work,omega,statevec,transmat, psi_tilda)
+  !! Deallocation of higher order tensors
+  deallocate (psi,psi0,ham,tkin,vpot,u,work,omega,statevec,transmat,psitilda)
 end program tdse
 
 double complex function scalar(n,h,psi1,psi2)
   ! Calculate scalar product <psi1|psi2> on the grid
   ! using trapezoidal rule and the boundary conditions
+  ! <psi1|psi2> = int(psi1*(x)psi2(x)dx)
   
-  ! Input variables
+  !! Defining parameters and local variables
   integer, intent(in):: n
   double precision, intent(in) :: h
   double complex, intent(in) :: psi1(n), psi2(n)
-
-  ! Local variables
   integer :: igrid
 
-  scalar = (0d0,0d0) ! we dont add dcmplx in front? Toby 1/30
+  !! Calculating scalar product
+  scalar = 0.5d0*(conjg(psi1(1))*psi2(1) &
+       & + conjg(psi1(n))*psi2(n)) ! First and last point
   do igrid = 2, n-1
      scalar = scalar + conjg(psi1(igrid))*psi2(igrid)
-  end do
-  ! First and last point
-  scalar = scalar + 0.5d0*(conjg(psi1(1))*psi2(1) &
-       & + conjg(psi1(n))*psi2(n)) 
+  end do 
   scalar = scalar*h
-  
 end function scalar
