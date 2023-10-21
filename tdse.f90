8b6fd4f9b0170d80aa6a6174dd0e9910e0104bb5
Toby Nguyen <tobyvn@congo.ps.uci.edu>
2023-10-20 18:43:45 -0700

    Added mixed scalar product and Green's function

--- tdse.f90
+++ tdse.f90
@@ -109,16 +109,16 @@ program tdse
   do itsteps = 1, ntsteps
 !!$     call propagate(n, h, psi, tau, chi)
 !!$     call propagate_ab(n, h, psi, psi_old, tau, chi)     
-     psinorm = dble(sqrt(scalar(n,h,psi(1,2),psi)))
-     print *, 'Mixed norm before propagation =', psinorm
      psinorm = dble(sqrt(scalar(n,h,psi,psi)))
      print *, 'Norm before forward propagation =', psinorm
      psinorm = dble(sqrt(scalar(n,h,psi(1,2),psi(1,2))))
      print *, 'Norm before backward propagation =', psinorm
-     call ham_psi(n, h, psi, psi_old)
-     call ham_psi(n, h, psi(1,2), psi_old(1,2))
-     call propagate_trap(n, h, psi, tau, chi, psi_old)
-     call propagate_trap(n, h, psi(1,2), -tau, chi(1,2), psi_old(1,2))
+!!$     call ham_psi(n, h, psi, psi_old)
+!!$     call ham_psi(n, h, psi(1,2), psi_old(1,2))
+!!$     call propagate_trap(n, h, psi, tau, chi, psi_old)
+!!$     call propagate_trap(n, h, psi(1,2), -tau, chi(1,2), psi_old(1,2))
+     call propagate_green(n, h, psi, tau, chi)
+     call propagate_green(n, h, psi(1,2), -tau, chi(1,2))
 !!$     do i = 1, n/2
 !!$        work(i) = temp
 !!$        work(i) = work(n-i+1) 
@@ -631,3 +631,60 @@ subroutine dump_psi(n, h, i_unit, psi)
   end do
 end subroutine dump_psi
 
+subroutine propagate_green(n, h, psi, tau, chi)
+  !------------------------------------------------------------------------------
+  !------------------------------------------------------------------------------
+  !
+  !------------------------------------------------------------------------------
+  ! Description: e^(-iHt)=1-iHt propagate the wavefunction at time t on psi
+  ! on t + delta_t (using the Green's function)
+  ! psi(t + tau) = psi(t) - i*tau/2 H[psi(t) + psi(t + tau)] OR
+  ! [1 + i*tau/2 H]psi(t + tau) = [1 - i*tau/2 H]psi(t)
+  !------------------------------------------------------------------------------
+  !
+  implicit none
+  !------------------------------------------------------------------------------
+  ! Input Parameters
+  !------------------------------------------------------------------------------
+  integer, intent(in):: n
+  double precision, intent(in):: h, tau
+  double complex, intent(in):: psi(n)
+  !------------------------------------------------------------------------------
+  ! Output Parameters
+  !------------------------------------------------------------------------------
+  double complex, intent(out):: chi(n)
+  !------------------------------------------------------------------------------
+  ! Input/Output Parameters
+  !------------------------------------------------------------------------------
+
+  !------------------------------------------------------------------------------
+  !  Local Variables
+  !------------------------------------------------------------------------------
+  integer :: i, j
+!!$  double complex, allocatable ::
+!!$  integer, allocatable :: 
+  double complex :: tmp, tmp2, xtemp, xptemp, prefactor
+  double complex, external :: scalar
+  !------------------------------------------------------------------------------
+  !  Local Constants 
+  !------------------------------------------------------------------------------
+  double precision, parameter :: pi = 4d0*atan(1d0)
+  
+!!$  allocate()
+  
+  do i=1,n
+     xtemp = dble(i)*h - 1d0
+     tmp2 = dcmplx(0d0,0d0)
+     do j=1,n
+        xptemp = dble(j)*h - 1d0
+        tmp = exp(-(xtemp-xptemp)**2/dcmplx(0d0,2d0*tau))
+        tmp2 = tmp2 + tmp*psi(j)
+     end do
+     chi(i) = tmp2 
+  end do
+  prefactor = 1/sqrt(dcmplx(0d0,2*pi*tau))
+  chi = prefactor*h*chi
+  
+!!$  deallocate()
+
+end subroutine propagate_green
