4a0a2387d99765e32c1464a20b0c10e45c55b879
Toby Viet Nguyen <tobyvn@uci.edu>
2024-05-10 18:12:54 -0700

    Fixed pval pvar boundaries and added subroutine for Wigner distr

--- tdse.f90
+++ tdse.f90
@@ -228,19 +228,17 @@ double precision function pval(n,h,idebug,psi)
   double complex :: tmp
   integer :: igrid
 
-!!$  pval = aimag(conjg(psi(1))*psi(2))/(2d0*h) &
-!!$       & - aimag(conjg(psi(n))*psi(n-1))/(2d0*h) ! First and last point
-  pval = 0d0
-  do igrid = 1, n
+  pval = aimag(conjg(psi(1))*psi(2))/(2d0*h) &
+       & - aimag(conjg(psi(n))*psi(n-1))/(2d0*h) ! First and last point
+  do igrid = 2, n-1
      pval = pval + aimag(conjg(psi(igrid))*(psi(igrid+1)-psi(igrid-1))/(2d0*h))
   end do 
   pval = pval*h
   
   if (idebug == 1) then
-!!$     tmp = conjg(psi(1))*psi(2)/(2d0*h) &
-!!$          & - conjg(psi(n))*psi(n-1)/(2d0*h) ! First and last point
-     tmp = 0d0
-     do igrid = 1, n
+     tmp = conjg(psi(1))*psi(2)/(2d0*h) &
+          & - conjg(psi(n))*psi(n-1)/(2d0*h) ! First and last point
+     do igrid = 2, n-1
         tmp = tmp + conjg(psi(igrid))*(psi(igrid+1)-psi(igrid-1))/(2d0*h)
      end do
      print *, 'Brute force momentum =',tmp*h
@@ -258,10 +256,9 @@ double precision function pvar(n,h,psi)
   
   ! var^2 = expt[(p-expt(p))^2] = (abs(p*psi - expt(p)*psi))^2
   tmp = pval(n,h,0,psi)
-!!$  pvar = (abs((psi(2))/(2d0*h) - tmp*psi(1)))**2 &
-!!$       & + (abs((-psi(n-1))/(2d0*h) - tmp*psi(n)))**2 ! First and last point
-  pvar = 0d0
-  do igrid = 1, n
+  pvar = (abs((psi(2))/(2d0*h) - tmp*psi(1)))**2 &
+       & + (abs((-psi(n-1))/(2d0*h) - tmp*psi(n)))**2 ! First and last point
+  do igrid = 2, n-1
      pvar = pvar + (abs(((psi(igrid+1)-psi(igrid-1)))/(2d0*h) - tmp*psi(igrid)))**2
   end do
   pvar = sqrt(pvar*h)
@@ -691,66 +688,66 @@ subroutine propagate_green(n, h, psi, tau, chi)
 
 end subroutine propagate_green
 
-subroutine propagate_fft(n, h, psi, tau, chi)
-  !------------------------------------------------------------------------------
-  !------------------------------------------------------------------------------
-  !
-  !------------------------------------------------------------------------------
-  ! Description: 
-  !------------------------------------------------------------------------------
-  !
-  use mkl_dfti
-  implicit none
-  !------------------------------------------------------------------------------
-  ! Input Parameters
-  !------------------------------------------------------------------------------
-  integer, intent(in):: n
-  double precision, intent(in):: h, tau
-  double complex, intent(in):: psi(n)
-  !------------------------------------------------------------------------------
-  ! Output Parameters
-  !------------------------------------------------------------------------------
-  double complex, intent(out):: chi(n)
-  !------------------------------------------------------------------------------
-  ! Input/Output Parameters
-  !------------------------------------------------------------------------------
-
-  !------------------------------------------------------------------------------
-  !  Local Variables
-  !------------------------------------------------------------------------------
-  integer :: i, j, istatus
-  type(dfti_descriptor), pointer :: My_Desc1_Handle, My_Desc2_Handle
-  double precision :: tmp, tmp_expt
-  double precision, parameter :: pi = 4d0*atan(1d0)
-  double complex, allocatable :: tmpfft(:)
-  !------------------------------------------------------------------------------
-  !  Local Constants 
-  !------------------------------------------------------------------------------
-
-  allocate(tmpfft(n))
-  
-  istatus = DftiCreateDescriptor(My_Desc1_Handle, DFTI_DOUBLE, DFTI_COMPLEX, 1, n)
-  istatus = DftiSetValue(My_Desc1_Handle, DFTI_PLACEMENT, DFTI_NOT_INPLACE)
-  istatus = DftiCommitDescriptor(My_Desc1_Handle)
-  istatus = DftiComputeForward(My_Desc1_Handle, psi, tmpfft) ! forward FFT
-
-  tmp_expt = 0d0
-!  
-  do i = 1,n
-     tmp = sin(2d0*pi*dble(i-1)/dble(n))/h ! momentum eigenvalue
-     tmp_expt = tmp_expt + abs(tmpfft(i))**2*tmp ! momentum expectation
-     ! value in energy basis
-     tmpfft(i) = tmpfft(i)/dble(n)*exp(cmplx(0d0,-tmp*tmp*tau*0.5d0)) ! propagated in energy basis
-  end do
-
-  
-  
-  istatus = DftiComputeBackward(My_Desc1_Handle, tmpfft, chi) ! backward FFT
-  istatus = DftiFreeDescriptor(My_Desc1_Handle)
-! result is given by {X_out(1),X_out(2),...,X_out(32)}
-  print *, 'momentum expectation value in energy basis =', h*tmp_expt/dble(n)
-  deallocate(tmpfft)
-end subroutine propagate_fft
+!!$subroutine propagate_fft(n, h, psi, tau, chi)
+!!$  !------------------------------------------------------------------------------
+!!$  !------------------------------------------------------------------------------
+!!$  !
+!!$  !------------------------------------------------------------------------------
+!!$  ! Description: 
+!!$  !------------------------------------------------------------------------------
+!!$  !
+!!$  use mkl_dfti
+!!$  implicit none
+!!$  !------------------------------------------------------------------------------
+!!$  ! Input Parameters
+!!$  !------------------------------------------------------------------------------
+!!$  integer, intent(in):: n
+!!$  double precision, intent(in):: h, tau
+!!$  double complex, intent(in):: psi(n)
+!!$  !------------------------------------------------------------------------------
+!!$  ! Output Parameters
+!!$  !------------------------------------------------------------------------------
+!!$  double complex, intent(out):: chi(n)
+!!$  !------------------------------------------------------------------------------
+!!$  ! Input/Output Parameters
+!!$  !------------------------------------------------------------------------------
+!!$
+!!$  !------------------------------------------------------------------------------
+!!$  !  Local Variables
+!!$  !------------------------------------------------------------------------------
+!!$  integer :: i, j, istatus
+!!$  type(dfti_descriptor), pointer :: My_Desc1_Handle, My_Desc2_Handle
+!!$  double precision :: tmp, tmp_expt
+!!$  double precision, parameter :: pi = 4d0*atan(1d0)
+!!$  double complex, allocatable :: tmpfft(:)
+!!$  !------------------------------------------------------------------------------
+!!$  !  Local Constants 
+!!$  !------------------------------------------------------------------------------
+!!$
+!!$  allocate(tmpfft(n))
+!!$  
+!!$  istatus = DftiCreateDescriptor(My_Desc1_Handle, DFTI_DOUBLE, DFTI_COMPLEX, 1, n)
+!!$  istatus = DftiSetValue(My_Desc1_Handle, DFTI_PLACEMENT, DFTI_NOT_INPLACE)
+!!$  istatus = DftiCommitDescriptor(My_Desc1_Handle)
+!!$  istatus = DftiComputeForward(My_Desc1_Handle, psi, tmpfft) ! forward FFT
+!!$
+!!$  tmp_expt = 0d0
+!!$!  
+!!$  do i = 1,n
+!!$     tmp = sin(2d0*pi*dble(i-1)/dble(n))/h ! momentum eigenvalue
+!!$     tmp_expt = tmp_expt + abs(tmpfft(i))**2*tmp ! momentum expectation
+!!$     ! value in energy basis
+!!$     tmpfft(i) = tmpfft(i)/dble(n)*exp(cmplx(0d0,-tmp*tmp*tau*0.5d0)) ! propagated in energy basis
+!!$  end do
+!!$
+!!$  
+!!$  
+!!$  istatus = DftiComputeBackward(My_Desc1_Handle, tmpfft, chi) ! backward FFT
+!!$  istatus = DftiFreeDescriptor(My_Desc1_Handle)
+!!$! result is given by {X_out(1),X_out(2),...,X_out(32)}
+!!$  print *, 'momentum expectation value in energy basis =', h*tmp_expt/dble(n)
+!!$  deallocate(tmpfft)
+!!$end subroutine propagate_fft
 
 subroutine propagate_convert(n, h, psi, tau, chi)
   !------------------------------------------------------------------------------
@@ -824,3 +821,59 @@ subroutine propagate_convert(n, h, psi, tau, chi)
   
   deallocate(tmppsi)
 end subroutine propagate_convert
+
+subroutine wigner_distr(n, h, psi, tau, chi)
+  !------------------------------------------------------------------------------
+  !------------------------------------------------------------------------------
+  !
+  !------------------------------------------------------------------------------
+  ! Description: 
+  !------------------------------------------------------------------------------
+  ! Calculates the Wigner distribution using a triple nested loop: one
+  ! over x, k, and xi where...
+  ! W(x,k_n) = (2/pi)*sum[psi*(x+xi_m)psi(x-xi_m)exp(2pi*xi_m*k_n*i)]
+  ! from 1 to min(x,n-x)
+  implicit none
+  !------------------------------------------------------------------------------
+  ! Input Parameters
+  !------------------------------------------------------------------------------
+  integer, intent(in):: n
+  double precision, intent(in):: h, tau
+  double complex, intent(in):: psi(n)
+  !------------------------------------------------------------------------------
+  ! Output Parameters
+  !------------------------------------------------------------------------------
+  double complex, intent(out):: chi(n)
+  !------------------------------------------------------------------------------
+  ! Input/Output Parameters
+  !------------------------------------------------------------------------------
+
+  !------------------------------------------------------------------------------
+  !  Local Variables
+  !------------------------------------------------------------------------------
+  integer :: i, j, k, istatus
+  double complex :: tmp
+  double precision, parameter :: pi = 4d0*atan(1d0)
+  double complex, allocatable :: w(:)
+  !------------------------------------------------------------------------------
+  !  Local Constants 
+  !------------------------------------------------------------------------------
+
+  allocate(w(n))
+
+  open(unit = 70, file = "wigner")
+  do i = 1,n
+     do j = 1,n
+        tmp = dcmplx(0d0,0d0)
+        do k = 1,min(i,n-i) ! xi loop
+           tmp = tmp + conjg(psi(i+k))*psi(i-k)*exp(dcmplx(0d0,2d0*pi*k*j))
+        end do
+        tmp = tmp*dble(h*2d0/pi)
+        w(j) = tmp
+     end do
+     write(70) (w(j), j = 1,n)
+  end do
+  close(unit = 70, status = "keep")
+  
+  deallocate(w)
+end subroutine wigner_distr
