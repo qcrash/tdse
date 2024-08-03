e59967b60a93f725e5b8ae0933134f528d38fa78
Triet Ho <homt3@uci.edu>
2024-08-02 18:55:24 -0700

    add segal bargmann subroutine, check square integrability

--- tdse.f90
+++ tdse.f90
@@ -95,9 +95,9 @@ program tdse
   call dump_psi(n, h, 69, psi0)
   close(69)
 
-  write(filename, "(a7,i0)") "wigner_", 1
+  write(filename, "(a3,i0)") "sb_", 1
   open(70, file = filename)
-  call wigner_distr(n,h,70,psi,tau,chi)
+  call segal_bargmann(n,h,70,psi,tau,chi)
   close(70)
   
 !!$  !! Defining Hamiltonian operator
@@ -178,9 +178,9 @@ program tdse
      call dump_psi(n, h, 69, psi)
      close(69)
 
-     write(filename, "(a7,i0)") "wigner_", itsteps+1
+     write(filename, "(a3,i0)") "sb_", itsteps+1
      open(70, file = filename)
-     call wigner_distr(n,h,70,psi,tau,chi)
+     call segal_bargmann(n,h,70,psi,tau,chi)
      close(70)
   end do
   
@@ -923,3 +923,75 @@ subroutine wigner_distr(n, h, iunit, psi, tau, chi)
   
   deallocate(w)
 end subroutine wigner_distr
+
+
+
+subroutine segal_bargmann(n, h, iunit, psi, tau, chi)
+  !------------------------------------------------------------------------------
+  !------------------------------------------------------------------------------
+  !
+  !------------------------------------------------------------------------------
+  ! Description: 
+  !------------------------------------------------------------------------------
+  ! Calculates the Segal Bargmann trasnform using a triple nested loop: one
+  ! over x, k, and xi where...
+  ! psi_bar(x,z) = e^(0.25*z^2)*sum_m[(psi(xi_m)exp(-0.5*(z - xi_m)^2)]
+  ! from 1 to n
+  implicit none
+  !------------------------------------------------------------------------------
+  ! Input Parameters
+  !------------------------------------------------------------------------------
+  integer, intent(in):: n, iunit
+  double precision, intent(in):: h, tau
+  double complex, intent(in):: psi(n)
+  !------------------------------------------------------------------------------
+  ! Output Parameters
+  !------------------------------------------------------------------------------
+  double complex, intent(out):: chi(n)
+  !------------------------------------------------------------------------------
+  ! Input/Output Parameters
+  !------------------------------------------------------------------------------
+
+  !------------------------------------------------------------------------------
+  !  Local Variables
+  !------------------------------------------------------------------------------
+  integer :: i, j, k, istatus
+  double precision, parameter :: pi = 4d0*atan(1d0)
+  double complex, allocatable :: psi_bar(:,:)
+  character :: fmt*60, fmt0*40
+  double precision :: xi, eta, upsilon
+  double complex :: z, tmp
+  !------------------------------------------------------------------------------
+  !  Local Constants 
+  !------------------------------------------------------------------------------
+
+  allocate(psi_bar(n,n))
+  
+  write(fmt,*) "(f16.10,",n,"(2x, f16.10, 1x, f16.10))"
+  write(fmt0,*) "(i10,",n,"(2x, f16.10))"
+  
+  write(70,fmt0) n+1, (h*dble(j) - 1d0, j =1, n)
+  do i = 1,n ! x loop
+     upsilon = -1d0 + dble(2*i)/dble(n+1) ! real part of z
+     do j = 1,n ! k loop
+        tmp = dcmplx(0d0,0d0)
+        eta = -1d0 + dble(2*j)/dble(n+1) ! imaginary part of z
+
+        z = dcmplx(upsilon, eta)
+
+        do k = 1, n
+           xi = dble(2*k)/dble(n+1)
+           tmp = tmp + psi(k)*exp(-0.5d0*(z - dcmplx(xi,0d0))**2)
+        end do
+        
+        tmp = exp(0.25d0*z**2)*tmp
+        
+           
+        tmp = tmp*h
+        psi_bar(j,i) = tmp
+     end do
+     write(70,fmt) h*dble(i) - 1d0,(real(psi_bar(j,i)), aimag(psi_bar(j,i)), j = 1,n)
+  end do
+  
+  deallocate(psi_bar)
+end subroutine segal_bargmann
